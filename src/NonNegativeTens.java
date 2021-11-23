abfdef237697fdf8c217dff6b578a26889200516
ShivaniiSinghh <91026976+ShivaniiSinghh@users.noreply.github.com>
2021-11-23 21:48:22 +0530

    Add files via upload

---
+++ src/NonNegativeTens.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class NonNegativeTens {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int k=(n/10);
+        int a=(k%10);
+        System.out.println(a);
+    }
+}
+

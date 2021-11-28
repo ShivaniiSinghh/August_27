077476db6f43e31a587dc4cdabea88900949e39d
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2021-11-28 22:22:48 +0530

    Add files via upload

---
+++ src/FruitTaste.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Fruit {
+    String fruit_name;
+    String fruit_taste;
+    public void eat(String fruit_name, String fruit_taste){
+        System.out.println(fruit_name);
+        System.out.println(fruit_taste);
+    }
+}
+class Apple extends Fruit{
+    public void eat(String fruit_name, String fruit_taste) {
+        System.out.println(fruit_name);
+        System.out.println(fruit_taste);
+    }
+}
+class Orange extends Fruit{
+    public void eat(String fruit_name, String fruit_taste) {
+        System.out.println(fruit_name);
+        System.out.println(fruit_taste);
+    }
+}
+public class FruitTaste{
+    public static void main(String[] args) {
+       Fruit obj1 = new Apple();
+        obj1.eat("Apple","Sweet");
+        Fruit obj2 = new Orange();
+        obj2.eat("Orange","Savoury");
+    }
+}
+

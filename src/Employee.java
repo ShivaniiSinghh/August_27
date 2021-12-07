104d29f70f62b51651d4ec13af08a7008d5cd474
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2021-12-07 20:00:10 +0530

    Add files via upload

---
+++ src/Employee.java
@@ -0,0 +1,6 @@
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Name\tYear of Joining\t\tAddress\nAman\t\t2021\t\t\t" +
+                "Chandigarh\nSunil\t\t2000\t\t\tMathura\nRohan\t\t2010\t\t\tAgra");
+    }
+}

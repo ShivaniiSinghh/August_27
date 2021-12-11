79e7eaa460bfd9b3be0392e08dcff7db50dabf5b
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2021-12-11 18:39:57 +0530

    Add files via upload

---
+++ src/SleepThread.java
@@ -0,0 +1,21 @@
+public class SleepThread implements Runnable {
+    static Thread t1;
+    public static void main(String[] args) {
+        t1 = new Thread(new SleepThread());
+        t1.start();
+
+    }
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6)
+                try {
+                    t1.sleep(5000);
+                }
+            catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            System.out.println(i);
+        }
+    }
+}
